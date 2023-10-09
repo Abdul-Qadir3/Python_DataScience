de16093beb7d24f121ae46ba1ab131fd2c09cd17
Abdul-Qadir3 <aqadirsamdani@gmail.com>
2023-10-09 12:03:43 -0700

    operators

---
+++ 01_python/01_operators.py
@@ -0,0 +1,77 @@
+print("Abdul Qadir")
+print('Abdul Qadir')
+print('''Abdul 
+      Qadir''')
+
+#Operators
+print(2+4) #added using print operator
+
+print(2-4) #subtracted using print operator
+
+print(2*4) #multiplied using print operator
+
+print(2/4) #divided using print operator
+
+print(2//4) #divided using print operator
+
+print(2%4) #modulus using print operator
+
+print(2**4) #exponentiation using print operator    
+
+a = [1,2,3,4] #list of integer
+b = [1,2,3,5] #list of integer
+
+print(a)
+print(b)
+print(a+b)#list concatenate when used addition operator
+
+# print(a==b)#equal
+# print(a===b)
+
+#Assignment operator
+
+a = 10
+b = 20
+a = a+b
+print(a)
+
+a = 10
+
+a += 20 #adding the value of the variable a by 20 and then assigning the result back to the variable a
+print(a)
+
+a = 10
+
+a -= 20 #subtracting the value of the variable a by 20 and then assigning the result back to the variable a
+
+print(a)
+
+a = 10
+
+a *= 20 #multiplying the value of the variable a by 20 and then assigning the result back to the variable a
+
+print(a)
+
+a = 10
+
+a /= 20 #Dividing the value of the variable a by 20 and then assigning the result back to the variable a
+
+print(a)
+
+a = 10
+
+a //= 20 # floor division on the value of the variable a by 20 and then assigning the result back to the variable a
+
+print(a)
+
+a = 10
+
+a **= 20 # raising the value of the variable a to the 20th power and then assigning the result back to the variable a
+
+print(a)
+
+a = 10
+
+a %= 20 # remainder when the value of the variable a is divided by 20, and then assigning the remainder as the new value of a 
+
+print(a)
