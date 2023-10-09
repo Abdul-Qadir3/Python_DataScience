78b261eca8ed9f5f7c4aac749f3d93bc5a9f5aeb
Abdul-Qadir3 <aqadirsamdani@gmail.com>
2023-10-09 12:14:22 -0700

    variables

---
+++ 01_python/02_variables.py
@@ -0,0 +1,19 @@
+# variable(container) declaration
+a=5#integer 
+b="block color" #string
+c=5.5 #float
+
+print(a)
+
+#use of variables
+#when to perform calculations
+#to import the data and name the data
+
+#rules
+#always use small letters
+#donot use spaces insted use underscore 
+#donot use special characters
+#donot use function that are built in python
+#donot use numbers at the beggining
+#short name
+#discriptve
