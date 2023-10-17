af012bbb949e61c430eb8af27a4ef5ef7da99b90
Abdul-Qadir3 <aqadirsamdani@gmail.com>
2023-10-17 13:20:08 -0700

    EDA

---
+++ 01_python/12_types_error.py
@@ -0,0 +1,26 @@
+x =5
+if x >3:
+#print("x is greater than 3")#syntax error indentation error
+
+#if x >3:
+    print("y is greater than 3")#human error/logical error
+    
+#runtime or module not found error
+#print(np.sqrt(9))#numpy not imported
+
+#name error
+#type error
+#value error
+#index error
+#keyerror
+#attribute error    
+#module import error
+#zero divia=sion error
+#file not found error
+#import error
+#memory error
+#timeout Error
+#os error
+
+
+
